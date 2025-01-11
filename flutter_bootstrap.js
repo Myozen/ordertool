bac5c9e9ad2cd9f32f4f39f8b0c4956a9cf774d3
Myozen <Myozen@users.noreply.github.com>
2025-01-11 20:00:42 +0000

    deploy: 2c55b90f20c786b4bdd1720759367487ccec96b8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4053758158"
+    serviceWorkerVersion: "3912936473"
   }
 });
